27ae0cc7040b52181b1ae8d1b94751e3b105a031
TGeek <30396033+wpddmcmc@users.noreply.github.com>
2019-08-22 16:38:32 +0800

    Update image.c

--- src/image.c
+++ src/image.c
@@ -219,6 +219,22 @@ void draw_bbox(image a, box bbox, int w, float r, float g, float b)
     }
 }
 
+image **load_alphabet_fix(char path[256])
+{
+    int i, j;
+    const int nsize = 8;
+    image **alphabets = calloc(nsize, sizeof(image));
+    for(j = 0; j < nsize; ++j){
+        alphabets[j] = calloc(128, sizeof(image));
+        for(i = 32; i < 127; ++i){
+            char buff[256];
+            sprintf(buff, "%s/%d_%d.png",path, i, j);
+            alphabets[j][i] = load_image_color(buff, 0, 0);
+        }
+    }
+    return alphabets;
+}
+
 image **load_alphabet()
 {
     int i, j;
